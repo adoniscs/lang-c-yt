f02b98c7e5869ac85bf7ccfd59200560899b63e7
Adonis Silveira <desenvolvimento777@gmail.com>
2026-05-10 18:29:28 -0300

    desenhar tabuleiro do jogo

--- projetos/jogo-da-velha.c
+++ projetos/jogo-da-velha.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void desenhaTabuleiro(char tabuleiro[3][3])
+{
+  system("cls");
+  printf("\n");
+
+  for (int i = 0; i < 3; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      printf(" %c ", tabuleiro[i][j]);
+      if (j < 2)
+      {
+        printf("|");
+      }
+    }
+    printf("\n");
+    if (i < 2)
+    {
+      printf("---+---+---\n");
+    }
+  }
+  printf("\n");
+}
+
 int main(int argc, char *argv[])
 {
+  char tabuleiro[3][3] = {
+      '1', '2', '3',
+      '4', '5', '6',
+      '7', '8', '9'};
+  int jogada = 0;
+  int posicao;
+
+  desenhaTabuleiro(tabuleiro);
+
   return 0;
 }
